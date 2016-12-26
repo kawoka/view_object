c2ca20129d51022e9da81c0d9fd79012acc80d70
kawaoka <br2jn51@yahoo.co.jp>
2016-12-26 13:25:54 +0900

    add before render

--- lib/view_object.rb
+++ lib/view_object.rb
@@ -1,11 +1,33 @@
 module ViewObject
   extend ActiveSupport::Concern
+
   included do
-    before_action {dispatch_view_object(self)}
+    define_callbacks :render
+    alias_method_chain :render, :before_render
+    before_render {before_render(self)}
+    before_action {before_action(self)}
   end
 
-  def dispatch_view_object(controller)
+  def before_action(controller)
     Dispatcher.dispatch_view_object(controller)
+  end
+
+  def before_render(controller)
+    Dispatcher.before_render
+  end
+
+  def render_with_before_render(*options, &block)
+    run_callbacks(:render) do
+      render_without_before_render(*options, &block)
+    end
+  end
+
+  module ClassMethods
+    def before_render(*names, &block)
+      _insert_callbacks(names, block) do |name, options|
+        set_callback(:render, :before, name, options)
+      end
+    end
   end
 
 end
--- lib/view_object/dispatcher.rb
+++ lib/view_object/dispatcher.rb
@@ -4,10 +4,22 @@ module ViewObject
 
     # @brief : Route毎にViewObjectをControllerとViewにDispatchする
     def self.dispatch_view_object(controller)
+      @view_object = make_view_object(controller)
       controller.instance_variable_set(
         :@view_object,
-        make_view_object(controller)
+        @view_object
       )
+    end
+
+    def self.callback method
+      return if @view_object.blank?
+      return unless @view_object.respond_to?(method)
+
+      @view_object.send(method)
+    end
+
+    def self.before_render
+      callback 'before_render'
     end
 
     def self.make_view_object(controller)
